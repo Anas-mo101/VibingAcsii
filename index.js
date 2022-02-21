a9e50166b5edea772b5af9b538dd30181dabd0b0
unknown <1201302614@student.mmu.edu.my>
2022-02-21 21:31:13 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app);
+const bodyparser = require('body-parser');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const {v4:uuidv4} = require('uuid');
+const bcrypt = require('bcrypt');
+
+const port = process.env.PORT || 3000;
+
+mongoose.connect(`mongodb+srv://XXXX:XXXXX@clusteriot.h9pwp.mongodb.net/VA?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+    console.log('Database connection successful');
+    server.listen(port, () => console.info(`listening on port ${port}`));
+})
+.catch(err => {
+    console.error('Database connection error: ' + err);
+});
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extened: true}));
+
+app.use(session({
+    secret: uuidv4(),
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(express.static('public'));
+app.use("/css" , express.static(__dirname + "public/css"));
+app.use("/js" , express.static(__dirname + "public/js"));
+app.use("/media" , express.static(__dirname + "public/media"));
+app.set('views', "./views");
+app.set('view engine', 'ejs');
+
+// ============ get =============
+
+app.get('/dashboard', async (req, res) => { 
+    res.render("dashboard"); 
+});
+
+app.get('/', async (req, res) => { 
+    res.render("frontpage"); 
+});
+
+app.get('/gallery', async (req, res) => { 
+    res.render("gallery"); 
+});
+
+app.get('/login', async (req, res) => { 
+    res.render("login"); 
+});
+
+app.get('/holders', async (req, res) => { 
+    res.render("holders"); 
+});
+
+app.get('/single', async (req, res) => { 
+    res.render("single"); 
+});
+
+// ============== post ===================
+
+app.post('/??', async (req, res) => { 
+
+});
---
+++ public/js/sec-2.js
@@ -0,0 +1,54 @@
+const S = 500
+
+let dt = 0
+
+const cursor = {
+  x: 0, y: 0
+}
+
+function setup() {
+  let canvas = createCanvas(windowWidth, S,).parent("lineCanvas");
+  background(0)
+  // canvas.position(0,0,)
+}
+
+function draw() {
+  background(0)
+  stroke(255)
+  strokeWeight(8)
+  
+  cursor.x = lerp(cursor.x, mouseX, 0.1)
+  cursor.y = lerp(cursor.y, mouseY, 0.1)
+  
+  fill(255)
+  circle(cursor.x, cursor.y, 8)
+  noFill(255)
+  
+  const p1 = {
+    x: -10, y: S/2
+  }
+  
+  const p2 = {
+    x: cursor.x - 100, 
+    y: cursor.y + 100 * Math.sin(dt/ 50)
+  }
+  
+  const p3 = {
+    x: cursor.x + 100, 
+    y: cursor.y + 100 * Math.cos(dt/ 50)
+  }
+  
+  const p4 = {
+    x: windowWidth,
+    y: S/2
+  }
+  
+  bezier(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y, p4.x, p4.y)
+  
+  strokeWeight(2)
+  line(p1.x, p1.y, p2.x, p2.y)
+  line(p2.x, p2.y, p3.x, p3.y)
+  line(p3.x, p3.y, p4.x, p4.y)
+  
+  dt += 1
+}
